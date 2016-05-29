2f571e94728ef93ce777eae0274e668f9ca0ad0c
serbi <syg.dam@gmail.com>
2016-05-29 19:28:42 +0200

    Delete clipboard.js

--- clipboard.js
+++
@@ -1,8 +0,0 @@
-var btn = document.getElementById('copy_btn');
-var clipboard = new Clipboard(btn);
-clipboard.on('success', function(e) {
-    console.log(e);
-});
-clipboard.on('error', function(e) {
-    console.log(e);
-});
